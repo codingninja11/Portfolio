8d6c9817bf9389d2755ab7656194f4813aab5f6f
codingninja11 <akritisheresth@gmail.com>
2021-08-30 01:36:27 +0530

    update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = (props) => (
           My Portfolio
         </SectionTitle>
         <SectionText>
-        The purpose of this Portfolio is compilation of academic work and other forms of educational evidence.
+        The purpose of this Portfolio is to compile academic work and other forms of educational evidence.
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
